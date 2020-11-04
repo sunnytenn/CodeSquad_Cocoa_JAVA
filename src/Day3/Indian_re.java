17eb6ca450224c4d9342fc972cd4a2c00ba81b4a
sunnytenn <sunnheee.park@gmail.com>
2020-11-04 15:00:19 +0900

    day3 추가

--- src/Day3/Indian_re.java
+++ src/Day3/Indian_re.java
@@ -7,15 +7,18 @@ import java.util.Scanner;
             System.out.println("생년월일을 입력해 주세요>");
             String birthyeardate = sc.nextLine();
             sc.close();
-            int pieceyear = Integer.parseInt(birthyeardate.substring(3));
+            int pieceyear = Integer.parseInt(birthyeardate.substring(3,4));
             int piecemonth = Integer.parseInt(birthyeardate.substring(4,6));
             int piecedate = Integer.parseInt(birthyeardate.substring(6, 8));
             piecemonth--;
             piecedate--;
-            String[] yearname = {"시끄러운", "말 많은", "푸른", "적색", "조용한", "웅크린", "백색", "지혜로운", "용감한", "날카로운", "욕심 많은"};
-            String[] monthname = {"의 뜻이 있는", "의 멋진"};
-            String[] dayname = {"의 멋진", "의 뛰어난"};
-            System.out.println("당신의 이름은"+yearname[pieceyear]+monthname[piecemonth]+dayname[piecedate]);
+            String[] yearname = {"시끄러운", "푸른", "적색", "조용한", "웅크린", "백색", "지혜로운", "용감한", "날카로운", "욕심 많은"};
+            String[] monthname = {"늑대", "태양","양","매","황소","불꽃","나무","달빛","말","돼지","하늘","바람"};
+            String[] dayname = {"와 함께 춤을","의 기상","은(는) 그림자속에"," "," "," ","의 환생","의 죽음","아래에서","를(을) 보라","이(가) 노래하다.","~의 그림자",
+                                "의 일격", "에게 쫓기는 남자","의 행진","의 왕","의 유령","을 죽인자.", "는(은) 맨날 잠잔다.","처럼..","의 고향","의 전사","은(는) 나의친구",
+                                "의 노래","의 정령","의 파수꾼","의 악마", "와(과)같은 사나이", "를(을) 쓰러트린자","의 혼","은(는) 말이없다."};
+
+            System.out.println("당신의 이름은"+" "+ yearname[pieceyear]+" "+monthname[piecemonth]+" "+dayname[piecedate]+"입니다");
 
         }
     }
---
+++ src/Day3/day3_summary.md
@@ -0,0 +1,20 @@
+#${cocoa}
+{학습 내용 정리하는 github: https://github.com/sunnytenn/CodeSquad_Cocoa_JAVA}
+
+### Day 03 
+
+#### public, private protected defualt의 차이..
+https://mainia.tistory.com/5574
+
+#### 프로그래머스 강의 배열, 클래스, 메소드 
+
+1.string class는 new연산자를 사용하지 않고도 인스턴스를 만들 수 있다. 
+-상수가 저장되는 영역에 저장, 상수는 변하지않는 값 
+-동시에 같은 값을 참조하게 됨 
+
+2.new연산자로도 생성은 할 수 있다. 
+-상수영역 참조 하지않음, new 연산자 사용해서 각각 새로운 인스턴스 생성함 
+참조 영역일때는 '=' 을 사용해서 비교할때 실제 값이 같은지가 아니라 주소가 같은지 확인하는 것이다. 
+
+#### scanner 활용 
+https://maktooob.tistory.com/17
