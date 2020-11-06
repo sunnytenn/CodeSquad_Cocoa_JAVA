476a482c32183f71acb57234c46cb17233a9a846
sunnytenn <sunnheee.park@gmail.com>
2020-11-06 11:20:52 +0900

    day4 추가

---
+++ src/Day5/Sol01.java
@@ -0,0 +1,12 @@
+package Day5;
+import java.util.Scanner;
+public class Sol01 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int A = sc.nextInt();
+        int B =sc.nextInt();
+        sc.close();
+        System.out.println(A+B);
+        }
+    }
+
